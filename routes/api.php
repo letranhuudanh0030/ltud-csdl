9277bf79ab6c77e18b8d82a2d7bf540f93ea1a29
letranhuudanh0030 <letranhuudanh0030@gmail.com>
2020-08-03 19:50:09 +0700

    update ssl v3

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+if (env('APP_ENV') === 'production') {
+    URL::forceSchema('https');
+}
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
