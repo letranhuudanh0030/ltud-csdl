32876dc4761a736b0ed971b3576a43b42509a210
letranhuudanh0030 <letranhuudanh0030@gmail.com>
2020-08-03 20:36:05 +0700

    update ssl v5

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,11 +23,7 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        if (env('APP_ENV') === 'production') {
-            URL::forceSchema('https');
-        }
-        
+    {     
         Schema::defaultStringLength(191);
     }
 }
