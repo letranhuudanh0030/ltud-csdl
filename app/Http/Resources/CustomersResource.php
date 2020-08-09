90f2bb17a7661e4b02a128c494ab0db160272533
letranhuudanh0030 <letranhuudanh0030@gmail.com>
2020-08-09 16:30:57 +0700

    update api customer

--- app/Http/Resources/CustomersResource.php
+++ app/Http/Resources/CustomersResource.php
@@ -15,6 +15,21 @@ class CustomersResource extends JsonResource
     public function toArray($request)
     {
         // return parent::toArray($request);
+
+        $event = $this->event;
+        if(!$event->first()['name']){
+            $eventArr = [];
+        } else {
+            $eventArr = [
+                'name' => $this->event->first()['name'],
+                'time_start' => $this->event->first()['time_start'],
+                'time_end' => $this->event->first()['time_end'],
+                'summary' => $this->event->first()['summary'],
+                'result' => $this->event->first()['result'],
+                'status' => $this->event->first()['status']
+            ];
+        }
+
         return [
             'name' => $this->name,
             'phone' => $this->phone,
@@ -22,14 +37,7 @@ class CustomersResource extends JsonResource
             'company' => $this->company,
             'address' => $this->address,
             'status' => $this->status,
-            'event' => [
-                'name' => $this->event->first()['name'],
-                'time_start' => $this->event->first()['time_start'],
-                'time_end' => $this->event->first()['time_end'],
-                'summary' => $this->event->first()['summary'],
-                'result' => $this->event->first()['result'],
-                'status' => $this->event->first()['status']
-            ]
+            'event' => $eventArr
         ];
     }
 }
