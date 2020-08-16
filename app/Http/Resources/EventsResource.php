c6b00b56fe61314bd9c3e7e03fc29d064f94a2f0
letranhuudanh0030 <letranhuudanh0030@gmail.com>
2020-08-16 12:53:20 +0700

    update event

--- app/Http/Resources/EventsResource.php
+++ app/Http/Resources/EventsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Event;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class EventsResource extends JsonResource
@@ -14,6 +15,29 @@ class EventsResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this['name'],
+            'time_start' => $this->time_start,
+            'time_end' => $this->time_end,
+            'summary' => $this->summary,
+            'result' => $this->result,
+            'status' => $this->status,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'customter' => [
+                'id' => $this->customer->id,
+                'name' => $this->customer->name,
+                'phone' => $this->customer->phone,
+                'email' => $this->customer->email,
+                'company' => $this->customer->company,
+                'address' => $this->customer->address,
+                'status' => $this->customer->status,
+                'created_at' => $this->customer->created_at,
+                'updated_at' => $this->customer->updated_at,
+
+            ],
+            'users' => $this->user 
+        ];
     }
 }
